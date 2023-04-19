f16230fc40982a2d3a3fee8e089a99fbec564dcd
jeonghwan <ajh5251@gmail.com>
2023-04-19 14:50:21 +0900

    commit2

--- crud/settings.py
+++ crud/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [port-0-crispy-robot-1maxx2algn977pd.sel3.cloudtype.app]
 
 
 # Application definition
